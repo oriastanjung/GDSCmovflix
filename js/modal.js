c8603b77e2d98650fc7487f3c2ca8acd7def44de
O. Riastanjung <87843869+oriastanjung@users.noreply.github.com>
2021-10-29 12:32:15 +0700

    v2.5
    adding modal popup that show the detail page

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+// let modal = document.getElementById("modal");
+// let closeButton = document.getElementById("close-btn");
+
+// closeButton.addEventListener("click", () => {
+//     modal.style.display = "none";
+// });
--- js/script.js
+++ js/script.js
@@ -16,59 +16,83 @@ let moviesTitle;
 let moviesRating;
 let moviesDesc;
 
+const modalLoc = document.getElementById("modal");
+let moviesID = document.getElementById("movies");
 function GetMovies(link) {
   fetch(link)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results);
       ShowMovies(data.results);
     });
 }
-function GetMoviesHighRated(link) {
-  fetch(link)
+
+GetMovies(API_URL);
+
+let modalContent;
+let modalTitle;
+let modalDesc;
+let modalMovie;
+let modalThumbnail;
+let modalInfo;
+let modalCloseButton;
+
+
+
+function takeID(x) {
+  fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results);
-      ShowMoviesHighRated(data.results);
+      data.results.map((item) => {
+        
+        if (item.id === x) {
+          
+          modalLoc.innerHTML = "";
+          
+          modalLoc.style.display = "block";
+          modalContent = `<div class="modal-content">`;
+          modalCloseButton = `<div id="close-btn" class="close">&times;</div>`;
+          modalMovie = ` <div class="modal-movie">`;
+          modalThumbnail = `<div class="modal-movie-thumbnail" >`;
+          modalImg = `<img src="${IMG_URL}${item.poster_path}" alt="" srcset=""></div>`;
+          modalInfo = `<div class="modal-movie-info">`;
+          modalTitle = `<div class="modal-movie-info-title"> <h2>${item.title}</h2></div>`;
+          modalDesc = `<div class="modal-movie-info-description"><p>${item.overview}</p></div>`;
+
+          modalLoc.innerHTML +=
+            modalContent +
+            modalCloseButton +
+            modalMovie +
+            modalThumbnail +
+            modalImg +
+            modalInfo +
+            modalTitle +
+            modalDesc +
+            `</div></div></div>`;
+
+            
+            let closeButton = document.getElementById("close-btn");
+            closeButton.addEventListener("click", () => {
+              document.getElementById("modal").style.display = "none";
+            });
+        }
+      });
     });
 }
-GetMovies(API_URL);
-GetMoviesHighRated(API_URL_HIGH_RATED);
-
-function ShowMoviesHighRated(data) {
-  document.getElementById("moviePopular").innerHTML = "";
-
-  data.map((item) => {
-    movies = `<article class="moviesHigh">`;
-    moviesThumbnail = `<div class="moviesThumbnailHigh">`;
-    moviesImg = `<img class="moviesImgHigh" src="${IMG_URL}${item.poster_path}" alt="film1"></div>`;
-    moviesInfo = `<div class="moviesInfo">`;
-    moviesTitle = `<h2 class="moviesTitle">${item.title}</h2>`;
-    moviesRating = `<p class="moviesRating">Rating : &#9733;${item.vote_average}/10</p>`;
-    moviesDesc = `<p class="moviesDesc"><span><a target="blank" href="https://www.themoviedb.org/movie/${item.id}" >Read More in TMDB</a></span></p></div></article>`;
-
-    document.getElementById("moviePopular").innerHTML +=
-      movies +
-      moviesThumbnail +
-      moviesImg +
-      moviesInfo +
-      moviesTitle +
-      moviesRating +
-      moviesDesc;
-  });
-}
-
+closeButton.addEventListener("click", () => {
+  modalLoc.style.display = "none";
+});
 function ShowMovies(data) {
+  let i = 0;
   movie.innerHTML = "";
 
   data.map((item) => {
-    movies = `<article class="movies">`;
+    movies = `<article class="movies" onclick="takeID(${item.id})">`;
     moviesThumbnail = `<div class="moviesThumbnail">`;
     moviesImg = `<img class="moviesImg" src="${IMG_URL}${item.poster_path}" alt="film1"></div>`;
     moviesInfo = `<div class="moviesInfo">`;
     moviesTitle = `<h2 class="moviesTitle">${item.title}</h2>`;
     moviesRating = `<p class="moviesRating">Rating : &#9733;${item.vote_average}/10</p>`;
-    moviesDesc = `<p class="moviesDesc">${item.overview}<span><a target="blank" href="https://www.themoviedb.org/movie/${item.id}" >Read More in TMDB</a></span></p></div></article>`;
+    moviesDesc = `<p class="moviesDesc">${item.overview}<span><a target="blank" href="https://www.themoviedb.org/movie/${item.id}" >Read More in TMDB</a></span></p>`;
 
     document.getElementById("movieLocate").innerHTML +=
       movies +
@@ -77,7 +101,16 @@ function ShowMovies(data) {
       moviesInfo +
       moviesTitle +
       moviesRating +
-      moviesDesc;
+      moviesDesc +
+      `</div></article>`;
+
+    document
+      .getElementsByClassName("movies")
+      [i].addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log(item.id, item.title, item.overview);
+      });
+    i++;
   });
 }
 
